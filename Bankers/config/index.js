878744d68987f92d799bccd30f11c5528f948f2d
SergejSKoreshkov <SergejSKoreshkov@gmail.com>
2019-04-02 22:04:45 +0300

    Web

--- Bankers/config/index.js
+++ Bankers/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Bankers-algorithm/',
 
     /**
      * Source Maps
